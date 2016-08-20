4c7e9eaffad27686a55996cd587e562a848e05ab
pablochux15@gmail.com <pablochux15@gmail.com>
2016-08-20 17:35:04 +0200

    Animate the appeareance of the search-form and when a search is performed
    Body in detail
        # This question tells reviewers of your pull request what to expect in the commit, allowing them to more easily identify and point out unrelated changes.
        # Describe, at a high level, what was done to affect change. Introduce a red/black tree to increase search speed or Remove <troublesome gem X>, which was causing <specific description of issue introduced by gem>are good examples.
        # If your change is obvious, you may be able to omit addressing this question.
        # This is the most important question to answer, as it can point out problems where you are making too many changes in one commit or branch. One or two bullet points for related changes may be okay, but five or six are likely indicators of a commit that is doing too many things.
        # Your team should have guidelines and rules-of-thumb for how much can be done in a single commit/branch.

--- template/src/js/script.js
+++ template/src/js/script.js
@@ -7,17 +7,13 @@ $(document).ready(function(){
             $('.dual-panel--down').hide();
             $('.search').removeClass('search--hide');
             setTimeout(function () {
-                // $('.search-form').removeClass('search-form--hide');
-                $('.search-result').show();
-                // $('.search-form__input').select();
+                $('.search-form').removeClass('search-form--hide');
+                $('.search-form__input').select();
             }, 10);
         }, 1000);
     });
-    $('.search-input__input').keypress(function() {
-        $('.search-input').addClass('animation');
-        console.log($(this).val());
-        if(!$(this).val()){
-            $('.search-input').removeClass('animation');
-        }
+    // Search-form move up animation
+    $('.search-form__icon').click(function() {
+        $('.search-form').addClass('search-form--up');
     });
 });
