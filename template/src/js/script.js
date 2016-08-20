f11a00b5a76d6e2e746f714f366b96c28ad5dbc8
pablochux15@gmail.com <pablochux15@gmail.com>
2016-08-20 15:18:15 +0200

    Add search-form layout and search-result layout

--- template/src/js/script.js
+++ template/src/js/script.js
@@ -5,6 +5,19 @@ $(document).ready(function(){
         setTimeout(function () {
             $('.dual-panel__link').hide();
             $('.dual-panel--down').hide();
+            $('.search').removeClass('search--hide');
+            setTimeout(function () {
+                // $('.search-form').removeClass('search-form--hide');
+                $('.search-result').show();
+                // $('.search-form__input').select();
+            }, 10);
         }, 1000);
+    });
+    $('.search-input__input').keypress(function() {
+        $('.search-input').addClass('animation');
+        console.log($(this).val());
+        if(!$(this).val()){
+            $('.search-input').removeClass('animation');
+        }
     });
 });
