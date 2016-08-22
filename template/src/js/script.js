e16d4a535621da4298ade06547543036b94782f4
pablochux15@gmail.com <pablochux15@gmail.com>
2016-08-22 03:22:48 +0200

    Implement the error cases of the search

--- template/src/js/script.js
+++ template/src/js/script.js
@@ -15,5 +15,27 @@ $(document).ready(function(){
     // Search-form move up animation
     $('.search-form__icon').click(function() {
         $('.search-form').addClass('search-form--expanded');
+        // Wikipedia API
+        performWikiSearch($('.search-form__input').val());
+        // Wikipedia API
     });
 });
+
+
+function performWikiSearch(search){
+    var api = "http://en.wsikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+    $.ajax({
+        type: "GET",
+        url: api + search + "&callback=?",
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function(data){
+            console.log(data);
+        },
+        error: function(data){
+            console.log(data);
+            $('.search-error').show().addClass('search-error--active');
+        }
+    });
+}
