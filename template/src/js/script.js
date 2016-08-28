cd4bc194f59de31ebce85f4ebf4a54689f5a938a
pablochux15@gmail.com <pablochux15@gmail.com>
2016-08-28 20:35:44 +0200

    Add auto-search and delete enter and icon click search

--- template/src/js/script.js
+++ template/src/js/script.js
@@ -2,9 +2,10 @@
 // functions
 // -------------------
 function displaySearchError(text) {
-    $('.search-result').append('<div class="search-error u-center"><h1 class="search-error__header">' + text + '</h1></div>');
+    search_result.append('<div class="search-error u-center"><h1 class="search-error__header">' + text + '</h1></div>');
 }
-function wikipediaApiSearch(search){
+
+function wikipediaApiSearch(search) {
     var api = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
     $.ajax({
         type: "GET",
@@ -12,73 +13,136 @@ function wikipediaApiSearch(search){
         contentType: "application/json; charset=utf-8",
         async: false,
         dataType: "json",
-        success: function(data){
-            $('.search-result').empty(); // Remove previous articles
+        success: function(data) {
+            search_result.empty(); // Remove previous articles
             console.log(data);
             console.log('Test');
             if (typeof data.query == 'object') {
                 displayArticles(data);
-            }else{
+            } else {
                 console.log('NOTHING');
                 displaySearchError('The search did not provide any results.');
             }
-            $('.footer').removeClass('footer--hide');
+            footer.removeClass('footer--hide');
         },
-        error: function(data){
+        error: function(data) {
             displaySearchError('The search could not be perfomed. </h1><h3 class="search-error__header">Please, try again in a few moments...</h3>');
         }
     });
 }
-
+/*
+ *   displayArticles -> query the data parameter (will be a json object from the ajax request)
+ *                      appending all the useful information intro search-result.
+ *                      And then, add the random-article button in the bottom of the page
+ *
+ *
+ */
 function displayArticles(data) {
-    for(var id in data.query.pages){
-        console.log(data.query.pages[id].title);
-        console.log(data.query.pages[id].extract);
-        $('.search-result').append("<div class='search-result-item'><a href='https://en.wikipedia.org/wiki/" + data.query.pages[id].title + "'><h1 class='search-result-item__title'>" + data.query.pages[id].title + "</h1><p class='search-result-item__body'>" + data.query.pages[id].extract + "</p></a></div>");
+    for (var id in data.query.pages) {
+        // console.log(data.query.pages[id].title);
+        // console.log(data.query.pages[id].extract);
+        search_result.append("<div class='search-result-item'><a href='https://en.wikipedia.org/wiki/" + data.query.pages[id].title + "'><h1 class='search-result-item__title'>" + data.query.pages[id].title + "</h1><p class='search-result-item__body'>" + data.query.pages[id].extract + "</p></a></div>");
     }
     // Add random article button
-    $('.search-result').append('<a href="https://en.wikipedia.org/wiki/Special:Random" class="search-result-item search-result-item--random-article u-center">Random Wikipedia Article</a>');
+    search_result.append('<a href="https://en.wikipedia.org/wiki/Special:Random" class="search-result-item search-result-item--random-article u-center">Random Wikipedia Article</a>');
 }
 
-function performWikiSearch(inputVal){
-    if(inputVal){
-        $('.search-form').addClass('search-form--expanded');
+function performWikiSearch(inputVal) {
+    if (inputVal) {
         // Wikipedia API
-        console.log('Input value: ' + inputVal);
-        wikipediaApiSearch($('.search-form__input').val());
-    }else{
-        $('.search-form__input').addClass('search-form__input--empty');
-        console.log('Without value in the search-form__input');
+        // console.log('Input value: ' + inputVal);
+        wikipediaApiSearch(inputVal);
+    } else {
+        search_input.addClass('search-form__input--empty');
+        // console.log('Without value in the search-form__input');
     }
 }
+// Usual elements
+var search = $('.search'),
+    search_form = $('.search-form'),
+    search_input = $('.search-form__input'),
+    search_icon = $('.search-form__icon'),
+    search_result = $('.search-result');
+
+var dualPanel_search = $('.dual-panel--search'),
+    dualPanel_link = $('.dual-panel__link'),
+    dualPanel_random = $('.dual-panel--random-article');
+
+var footer = $('.footer');
+
+// Other variables
+var keyTimeout;
+var doneTyppingInterval = 400; // in ms
+var doneTypping = false;
 
 // -------------------
 // Main script
 // -------------------
-$(document).ready(function(){
-    $('.dual-panel--search').click(function() {
-        $(this).addClass('dual-panel--active');
-        $('.dual-panel--search').removeClass('dual-panel--search');
+$(document).ready(function() {
+    /*
+     * Home page with a dual-panel:
+     *   Search: dual-panel--search will transform and adopt all the body width,
+     *           moving the dual-panel--random-article to the back, and then hide it.
+     *
+     *   Random Article -> will go to a Wikipedia Random Article
+     */
+    dualPanel_search.click(function() {
+        $(this).addClass('dual-panel--active').removeClass('dual-panel--search');
         // 1sec -> when the height animation is completed
-        setTimeout(function () {
-            $('.dual-panel__link').hide();
-            $('.dual-panel--random-article').hide();
-            $('.search').removeClass('search--hide');
-            console.log(this);
-            setTimeout(function () {
-                $('.search-form').removeClass('search-form--hide');
-                $('.search-form__input').select();
+        setTimeout(function() {
+            dualPanel_link.hide();
+            dualPanel_random.hide();
+            search.removeClass('search--hide');
+            // console.log(this);
+            setTimeout(function() {
+                search_form.removeClass('search-form--hide');
+                search_input.select();
             }, 10);
         }, 1000);
     });
-    // Search perfomed
-    $('.search-form__icon').click(function() {
-        performWikiSearch($('.search-form__input').val());
+
+    /*
+     *   The search will be perfomed when the user stops typing
+     *   The click in the icon or the enter key is not needed.
+     *
+     *
+     */
+    // Experiment
+    search_icon.click(function() {
+        performWikiSearch(search_input.val());
     });
-    $("input").keypress(function(event) {
-        if (event.which == 13) {
-            event.preventDefault();
-            performWikiSearch($('.search-form__input').val());
+    // End Experiment
+
+    // Experiment
+    // When keyup (suelta la tecla) -> empieza el cronometro para ver si pasan los segundos (si pasan, se hace la búsqueda en wikipedia)
+    // Pero si vuelve a presionar una tecla (keydown) -> se reinicia el cronometro y no se hace una búsqueda
+
+    search_input.keyup(function() {
+        // Expand the search-form after a key is pressed
+        search_form.addClass('search-form--expanded');
+
+        // Deletes all the search-result-items when the input is empty
+        if (!search_input.val()) {
+            search_result.empty();
+            search_form.removeClass('search-form--expanded');
         }
+
+        // Se pone un contador para comprobar si se presiona otra tecla, en el caso de que no
+        // se presione ninguna, el valor de doneTypping será verdadero por lo que se hará la búsqueda
+        keyTimeout = setTimeout(function() {
+            doneTypping = true;
+        }, doneTyppingInterval);
+
+        setTimeout(function() {
+            if (doneTypping) {
+                performWikiSearch(search_input.val());
+            }
+        }, doneTyppingInterval + 1);
     });
+
+    search_input.keydown(function() {
+        doneTypping = false;
+        clearTimeout(keyTimeout);
+    });
+    // End Experiment
 });
