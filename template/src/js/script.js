94499b0f322a0b7b52ab65037ee19898a03373ae
pablochux15@gmail.com <pablochux15@gmail.com>
2016-09-11 19:18:14 +0200

    Fix https request problem

--- template/src/js/script.js
+++ template/src/js/script.js
@@ -6,7 +6,7 @@ function displaySearchError(text) {
 }
 
 function wikipediaApiSearch(search) {
-    var api = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+    var api = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
     $.ajax({
         type: "GET",
         url: api + search + "&callback=?",
